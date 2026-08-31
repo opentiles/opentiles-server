e33c27f397bb1e71e182624addc03561e5483d8d
Ziv Perry <ziv.perry@gmail.com>
2026-08-31 18:52:28 +0300

    ci: publish to ecr

---
+++ publish-to-ecr.sh
@@ -0,0 +1,26 @@
+ECR_REGISTRY=667512625118.dkr.ecr.eu-north-1.amazonaws.com
+IMAGE_TAG=v0.2.0
+
+aws login --profile opentiles-deploy
+
+aws ecr get-login-password \
+  --region eu-north-1 \
+  --profile opentiles-deploy |
+docker login \
+  --username AWS \
+  --password-stdin "$ECR_REGISTRY"
+
+
+docker buildx build \
+  --platform linux/arm64 \
+  --load \
+  --progress=plain \
+  -t "opentiles-server:$IMAGE_TAG" \
+  .
+
+
+docker tag \
+  "opentiles-server:$IMAGE_TAG" \
+  "$ECR_REGISTRY/opentiles-server:$IMAGE_TAG"
+
+  docker push "$ECR_REGISTRY/opentiles-server:$IMAGE_TAG"
